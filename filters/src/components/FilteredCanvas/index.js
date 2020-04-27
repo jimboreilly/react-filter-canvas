199d9298d072c2a701f661abd3f1b64de6bce23e
JamesMReilly <jimreilly28@gmail.com>
2020-04-27 16:45:08 -0500

    draw an image from a source in the canvas using refs

--- filters/src/components/FilteredCanvas/index.js
+++ filters/src/components/FilteredCanvas/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const FilteredCanvas = () =>
-	<div>bar</div>
+const FilteredCanvas = ({ src }) => {
+	const canvas = useRef(null);
+
+	const test = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/800px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg"
+
+	useEffect(() => {
+		const current = canvas.current;
+		const context = current.getContext('2d')
+		const img = new Image();
+		img.src = test
+		img.onload = function () {
+			current.width = img.width;
+			current.height = img.height;
+			context.drawImage(img, 0, 0)
+		}
+	})
+
+
+	return (
+		<div>
+			<canvas id="test" ref={canvas}></canvas>
+		</div >
+	)
+}
 
 
 export default FilteredCanvas;
